695cf3c4a2b4cf67768e49875e21799f8b971b36
Siddharth Bhola <sbhola@tavisca.com>
2014-11-14 11:21:21 +0530

    Initialized mermaid repo

---
+++ Mermaid/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Mermaid
+{
+    partial class Meramid
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label_UserName = new System.Windows.Forms.Label();
+            this.textBox_UserName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Label_UserName
+            // 
+            this.Label_UserName.AutoSize = true;
+            this.Label_UserName.Font = new System.Drawing.Font("Mistral", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_UserName.Location = new System.Drawing.Point(52, 90);
+            this.Label_UserName.Name = "Label_UserName";
+            this.Label_UserName.Size = new System.Drawing.Size(332, 29);
+            this.Label_UserName.TabIndex = 0;
+            this.Label_UserName.Text = "Please enter your Name to continue :";
+            this.Label_UserName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox_UserName
+            // 
+            this.textBox_UserName.Location = new System.Drawing.Point(161, 149);
+            this.textBox_UserName.Name = "textBox_UserName";
+            this.textBox_UserName.Size = new System.Drawing.Size(100, 22);
+            this.textBox_UserName.TabIndex = 1;
+            this.textBox_UserName.Text = "Aladin";
+            // 
+            // Meramid
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 412);
+            this.Controls.Add(this.textBox_UserName);
+            this.Controls.Add(this.Label_UserName);
+            this.Name = "Meramid";
+            this.Text = "Mermaid";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label_UserName;
+        private System.Windows.Forms.TextBox textBox_UserName;
+    }
+}
+
